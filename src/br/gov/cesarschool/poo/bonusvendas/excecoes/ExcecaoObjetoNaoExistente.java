2a26a84833c3ee150899ac30fad6c9d1e834afe4
TomCabral <apbc@cesar.school>
2023-11-27 19:08:15 -0300

    Create ExcecaoObjetoNaoExistente.java

---
+++ src/br/gov/cesarschool/poo/bonusvendas/excecoes/ExcecaoObjetoNaoExistente.java
@@ -0,0 +1,9 @@
+package br.gov.cesarschool.poo.bonusvendas.excecoes;
+public class ExcecaoObjetoNaoExistente extends Exception {
+    public ExcecaoObjetoNaoExistente() {
+        super("Objeto nao existente");
+    }
+    public ExcecaoObjetoNaoExistente(String mensagem) {
+        super(mensagem);
+    }
+}
