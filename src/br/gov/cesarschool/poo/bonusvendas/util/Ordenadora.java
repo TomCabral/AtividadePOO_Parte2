17ceaa0f2810d946d518d539d97412c0707626a2
Adaury Neto <91835392+Adaurekt@users.noreply.github.com>
2023-11-28 16:39:46 -0300

    Update Ordenadora
    Alterações na classe Ordenadora

--- src/br/gov/cesarschool/poo/bonusvendas/util/Ordenadora.java
+++ src/br/gov/cesarschool/poo/bonusvendas/util/Ordenadora.java
@@ -1,16 +1,18 @@
 package br.gov.cesarschool.poo.bonusvendas.util;
 
+import br.gov.cesarschool.poo.bonusvendas.entidade.CaixaDeBonus;
+
 public class Ordenadora {
 
     private Ordenadora() {
         // Construtor privado para impedir instanciação da classe
     }
 
-    public static void ordenar(Object[] lista, Comparador comp) {
+    public static void ordenar(CaixaDeBonus[] lista, Comparador ComparadorCaixaDeBonusSaldoDec) {
         for (int i = 0; i < lista.length - 1; i++) {
             for (int k = i + 1; k < lista.length; k++) {
                 // Comparação utilizando o método comparar do Comparador
-                if (comp.comparar(lista[i], lista[k]) > 0) {
+                if (ComparadorCaixaDeBonusSaldoDec.comparar(lista[i], lista[k]) > 0) {
                     // Troca de posição caso o resultado da comparação seja maior que zero
                     trocarPosicao(lista, i, k);
                 }
@@ -18,8 +20,8 @@ public class Ordenadora {
         }
     }
 
-    private static void trocarPosicao(Object[] lista, int i, int k) {
-        Object temp = lista[i];
+    private static void trocarPosicao(CaixaDeBonus[] lista, int i, int k) {
+        CaixaDeBonus temp = lista[i];
         lista[i] = lista[k];
         lista[k] = temp;
     }
